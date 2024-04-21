a55644ac51b5dedec3acb2b4b477e75eb2b4cc76
JasonLing0524 <93655793+JasonLing0524@users.noreply.github.com>
2024-04-20 20:21:32 -0400

    comment on functions

--- original_state_fcn.m
+++ original_state_fcn.m
@@ -1,24 +1,24 @@
 function state_derivatives = original_state_fcn(states, inputs) % Vx, Vy, delta % f, theta
-S = 0.419; % wing surface
-b = 0.75; % wing span
-c = 0.317; % wing mean chord
-rho = 1.01; % air density
-beta_max = 30; % maximum angle of flapping wing
-l = 0.3; % distance between center of mass of ornithopter to center of tail
-St = 0.091; % tail surface
-bt = 0.46; % tail wingspan
-ARt = bt^2/St; % tail aspect ratio
-g = 9.8; % gravity constant
-m = 3.415; % total mass
-I = (4*l^2+0.01)*m/12; % Inertia
+S = 0.419;                                        % wing surface
+b = 0.75;                                         % wing span
+c = 0.317;                                        % wing mean chord
+rho = 1.01;                                       % air density
+beta_max = 30;                                    % maximum angle of flapping wing
+l = 0.3;                                          % distance between center of mass of ornithopter to center of tail
+St = 0.091;                                       % tail surface
+bt = 0.46;                                        % tail wingspan
+ARt = bt^2/St;                                    % tail aspect ratio
+g = 9.8;                                          % gravity constant
+m = 3.415;                                        % total mass
+I = (4*l^2+0.01)*m/12;                            % Inertia
 
-U = sqrt(states(1)^2+states(2)^2); % relative velocity
-gamma = atand(states(2)/states(1)); % fly path angle
-phi = states(3)-gamma; % angle of attack
-f = inputs(1); % wing flapping frequency
-theta = inputs(2); % tail wing angle
+U = sqrt(states(1)^2+states(2)^2);                % relative velocity
+gamma = atand(states(2)/states(1));               % fly path angle
+phi = states(3)-gamma;                            % angle of attack
+f = inputs(1);                                    % wing flapping frequency
+theta = inputs(2);                                % tail wing angle
 
-Re = U*c/1.5111/10^-5; % Reynolds number
+Re = U*c/1.5111/10^-5;                            % Reynolds number
 % integral of vertical forces from flapping wings over wing length and a flapping cycle (body frame)
 Vertical_wing = integral2(@(r, t) vertical(r, t, f, b, c, U, phi, beta_max, rho, Re), 0, b, 0, 1/f);
 % integral of horizontal forces from flapping wings over wing length and a flapping cycle (body frame)
