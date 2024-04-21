7512690f1293c1ef9b3f18bb6fae3bce74b131c5
JasonLing0524 <93655793+JasonLing0524@users.noreply.github.com>
2024-04-20 20:37:27 -0400

    name change

--- data_gathering.m
+++ data_gathering.m
@@ -3,22 +3,13 @@ close all;clear;clc;
 %% initialization
 sample_number = 150000;
 dt = 0.1;
-% % range
-% Vx_range = [0.1;5];
-% Vy_range = [-2;2];
-% delta_range = [-30;30];
-% delta_dot_range = [-100;100];
-% 
-% f_range = [0.1;5];
-% theta_range = [-5; 5];
-% range
-Vx_range = [15;25];
-Vy_range = [-5;5];
-delta_range = [-15;15];
-delta_dot_range = [-50;50];
+Vx_range = [15;25];                     % range of Vx
+Vy_range = [-5;5];                      % range of Vy
+delta_range = [-15;15];                 % range of delta
+delta_dot_range = [-50;50];             % range of deltadot
 
-f_range = [0.1;5];
-theta_range = [-10; 10];
+f_range = [0.1;5];                      % range of f
+theta_range = [-10; 10];                % range of theta
 % array
 Vx_arr = zeros(sample_number, 1);
 Vy_arr = zeros(sample_number, 1);
@@ -26,11 +17,12 @@ delta_arr = zeros(sample_number, 1);
 delta_dot_arr = zeros(sample_number, 1);
 f_arr = zeros(sample_number, 1);
 theta_arr = zeros(sample_number, 1);
-
+% next states
 state_next = zeros(sample_number, 4);
 %%  random states and input
 tic;
 parfor i = 1:sample_number
+    % uniform random
     Vx = unifrnd(Vx_range(1), Vx_range(2));
     Vy = unifrnd(Vy_range(1), Vy_range(2));
     delta = unifrnd(delta_range(1), delta_range(2));
@@ -47,7 +39,7 @@ parfor i = 1:sample_number
     
     states = [Vx;Vy;delta;delta_dot];
     inputs = [f; theta];
-    
+    % simulate with original continuous-time function to get next states
     [~,x_temp] = ode45(@(t,x) original_state_fcn(x,inputs) , [0 dt], states);
     state_next(i, :) = x_temp(end,:)';
 i
